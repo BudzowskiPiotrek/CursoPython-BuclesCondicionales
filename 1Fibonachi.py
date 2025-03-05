19e332c998ac87f2793e2632ecc8e1960e482436
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-05 17:34:20 +0100

    Update 1Fibonachi.py

--- 1Fibonachi.py
+++ 1Fibonachi.py
@@ -2,8 +2,27 @@
 a = 0
 b = 1
 
-for var in range(1, 11):
+for var in range(1, 10):
     c = a + b
     print(c, "", end="")
     a = b
     b = c
+
+# o
+
+a, b = 0, 1
+fibonacci = [a]
+
+for _ in range(10):
+    a, b = b, a + b
+    fibonacci.append(a)
+
+# _ La barra baja _ en for _ in range(n) es una convención en Python que indica que la variable no se usará dentro del bucle.
+print(fibonacci)
+
+# o
+
+print([" ".join(map(str, fibonacci))])
+
+# map(función, iterable) aplica la función str() a cada elemento de fibonacci, convirtiéndolos en texto.
+# " ".join(iterable) une los elementos de la lista en una sola cadena, separándolos con un espacio " ".
