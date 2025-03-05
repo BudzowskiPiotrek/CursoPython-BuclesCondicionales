924a5c4e8ccee54fd26a8a6d6c185e6317bad59d
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-05 17:58:30 +0100

    Create 3CalculadoraWhile.py

---
+++ 3CalculadoraWhile.py
@@ -0,0 +1,8 @@
+# haz una calculadora de tabla 2 primeros 10 numeros
+num1 = 2
+cont = 0
+while cont < 10:
+    cont += 1
+    suma = num1 * cont
+    print(num1, " * ", cont, " = ", suma)
+print("Programa ha terminado")
