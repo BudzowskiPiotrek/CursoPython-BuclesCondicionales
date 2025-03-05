9ac6c720fb718d6737769176f55ce54552e7b306
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-05 17:23:21 +0100

    Delete ejercicio1.py

--- ejercicio1.py
+++
@@ -1,9 +0,0 @@
-# Calcula e imprime los primeros 10 números de la sucesión de Fibonacci utilizando bucles y variables.
-a = 0
-b = 1
-
-for var in range(1, 11):
-    c = a + b
-    print(c, "", end="")
-    a = b
-    b = c
